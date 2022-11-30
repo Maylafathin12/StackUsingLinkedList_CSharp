92cc2ed75308e664bdf114035aa6c392d6904bc2
TA1069 <maylafaat@gmail.com>
2022-11-30 16:05:49 +0700

    add case 3

--- StackUsingLinkedList_CSharp/Program.cs
+++ StackUsingLinkedList_CSharp/Program.cs
@@ -84,8 +84,9 @@ namespace StackUsingLinkedList_CSharp
                         }
                         s.pop();
                         break;
-
-                }
+                    case '3':
+                        s.display();
+                        break;
             }
         }
     }
